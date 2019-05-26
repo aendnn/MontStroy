a9edf19eeb12e33ce81f48fb8ad89c00a7a25439
Anna <aendnn9@gmail.com>
2019-05-26 17:57:12 +0300

    Исправляет оптимизацию для карты

--- source/js/map.js
+++ source/js/map.js
@@ -1,22 +1,15 @@
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+'use strict';
 setTimeout(function(){
   var elem = document.createElement('script');
   elem.type = 'text/javascript';
-  elem.src = '//api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-RU&onload=getYaMap';
+  elem.src = '//api-maps.yandex.ru/2.0/?apikey=ec1eb4b4-3ffe-4975-a696-b52dc7702801&load=package.standard&lang=ru-RU&onload=getYaMap';
   document.getElementsByTagName('body')[0].appendChild(elem);
 }, 2000);
 
+
 function getYaMap(){
-  // Создание карты.
   var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
     center: [55.63150204, 37.61708150],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
     zoom: 16
   });
 
