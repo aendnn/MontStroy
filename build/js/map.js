102ab996070b650d421ed9bb762098bd679de75c
Anna <aendnn9@gmail.com>
2019-05-28 21:02:35 +0300

    Добавляет слайдер и мелкие правки

--- build/js/map.js
+++ build/js/map.js
@@ -1,5 +1,5 @@
 'use strict';
-setTimeout(function(){
+setTimeout(function () {
   var elem = document.createElement('script');
   elem.type = 'text/javascript';
   elem.src = '//api-maps.yandex.ru/2.0/?apikey=ec1eb4b4-3ffe-4975-a696-b52dc7702801&load=package.standard&lang=ru-RU&onload=getYaMap';
@@ -7,13 +7,13 @@ setTimeout(function(){
 }, 2000);
 
 
-function getYaMap(){
-  var myMap = new ymaps.Map("map", {
+function getYaMap() {
+  var myMap = new ymaps.Map('map', {
     center: [55.63150204, 37.61708150],
     zoom: 16
   });
 
-  var myPlacemark = new ymaps.Placemark([55.63172696, 37.61765126], {hasBaloon: false},{
+  var myPlacemark = new ymaps.Placemark([55.63172696, 37.61765126], {hasBaloon: false}, {
     iconLayout: 'default#image',
     iconImageHref: '../img/icon-marker.svg',
     iconImageSize: [30, 36]
