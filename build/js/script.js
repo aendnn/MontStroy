37d019395fc275a335f36a1faa585f45686fc7b7
Anna <aendnn9@gmail.com>
2019-05-28 14:07:51 +0300

    Добавляет функционал слайдеров

--- build/js/script.js
+++ build/js/script.js
@@ -43,42 +43,132 @@ svg4everybody();
 
 objectFitImages('img');
 
+var currentBreakpoint = '';
+var swiper;
+var swiperFeatures;
 
-var partnersSwiper = new Swiper('.swiper-container', {
+var desktopSwiperConfig = {
+  init: false,
   slidesPerView: 4,
   spaceBetween: 30,
   loop: true,
   navigation: {
-    nextEl: '.arrow--next',
-    prevEl: '.arrow--prev',
+    nextEl: '.arrow--partners-next',
+    prevEl: '.arrow--partners-prev',
   },
+};
+
+var desktopSwiperConfigFeatures = {
+  init: false,
+  on: {
+    resize: function () {
+      if (window.matchMedia('(min-width: 1200px)').matches) { this.destroy(); }
+    }
+  }
+};
+
+var tabletSwiperConfig = {
+  init: false,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  centeredSlides: true,
+  effect: 'coverflow',
+  loop: true,
   pagination: {
     el: '.partners__toggles',
     clickable: true,
-    dynamicBullets: true,
   },
-  breakpoints: {
-    1200: {
-      spaceBetween: 0,
-      slidesPerView: 'auto',
-      slidesPerGroup: 3,
-      initialSlide: 2,
-      centeredSlides: true,
-      pagination: {
-        el: '.partners__toggles',
-        clickable: true,
-        dynamicBullets: true
-      },
-    },
-    768: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      loop: false,
-      initialSlide: 1,
-      centeredSlides: false,
-      pagination: {
-        dynamicBullets: true,
-      },
-    }
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
   }
-});
+};
+
+var tabletSwiperConfigFeatures = {
+  init: false,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  centeredSlides: true,
+  effect: 'coverflow',
+  loop: true,
+  pagination: {
+    el: '.features__toggles',
+    clickable: true,
+  },
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  }
+};
+
+
+var mobileSwiperConfig = {
+  init: false,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  centeredSlides: false,
+  pagination: {
+    el: '.partners__toggles',
+    clickable: true,
+  },
+};
+
+var mobileSwiperConfigFeatures = {
+  init: false,
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 0,
+  pagination: {
+    el: '.features__toggles',
+    clickable: true,
+  },
+};
+
+function initSwiper () {
+  if (window.matchMedia('(max-width: 767px)').matches && currentBreakpoint !== 'mobile') {
+    currentBreakpoint = 'mobile';
+
+    if (swiper) swiper.destroy();
+    swiper = new Swiper('.swiper-container', mobileSwiperConfig);
+    swiper.init();
+
+    if (swiperFeatures) swiperFeatures.destroy();
+    swiperFeatures = new Swiper('.swiper-container-features', mobileSwiperConfigFeatures);
+    swiperFeatures.init();
+  }
+
+  if (window.matchMedia('(min-width: 768px) and (max-width: 1199px)').matches && currentBreakpoint !== 'tablet') {
+    currentBreakpoint = 'tablet';
+
+    if (swiper) swiper.destroy();
+    swiper = new Swiper('.swiper-container', tabletSwiperConfig);
+    swiper.init();
+
+    if (swiperFeatures) swiperFeatures.destroy();
+    swiperFeatures = new Swiper('.swiper-container-features', tabletSwiperConfigFeatures);
+    swiperFeatures.init();
+  }
+
+  if (window.matchMedia('(min-width: 1200px)').matches && currentBreakpoint !== 'desktop') {
+    currentBreakpoint = 'desktop';
+
+    if (swiper) swiper.destroy();
+    swiper = new Swiper('.swiper-container', desktopSwiperConfig);
+    swiper.init();
+
+    if (swiperFeatures) swiperFeatures.destroy();
+    swiperFeatures = new Swiper('.swiper-container-features', desktopSwiperConfigFeatures);
+    swiperFeatures.init();
+  }
+}
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
